2e40a7f3131bf7b575e24839cc352a5098f74b08
shisir <shisirhumagain0@gmail.com>
2024-10-16 21:16:00 -0400

    files added

--- src/main/java/com/graphqljava/project/bookDetails/Book.java
+++ src/main/java/com/graphqljava/project/bookDetails/Book.java
@@ -6,9 +6,9 @@ import java.util.List;
 public record Book(String id, String name, int pageCount, String authorId) {
 
     private static List<Book> books = Arrays.asList(
-            new Book("book-1", "Harry Potter and the Philosopher's Stone", 223, "Shisir"),
-            new Book("book-2","Moby Dick",635,"Ramila"),
-            new Book("book-3", "Interview with the vampire",371, "Basanta")
+            new Book("book-1", "Harry Potter and the Philosopher's Stone", 223, "author-1"),
+            new Book("book-2","Moby Dick",635,"author-2"),
+            new Book("book-3", "Interview with the vampire",371, "author-3")
     );
 
     public static Book getById(String id){
